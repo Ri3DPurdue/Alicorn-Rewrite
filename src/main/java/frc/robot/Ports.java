cd228dd6be19911f5bede5699a32f7dbff2633e1
Daniel Quednow <danquednow@gmail.com>
2025-11-01 14:42:44 -0400

    Add basic drivetrain

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -4,7 +4,12 @@ public enum Ports {
     INTAKE_PIVOT_MAIN(8, "rio"),
     INTAKE_PIVOT_FOLLOWER(9, "rio"),
     INTAKE_ROLLERS(10, "rio"),
-    INTAKE_INDEXER(11, "rio");
+    INTAKE_INDEXER(11, "rio"),
+    
+    DRIVETRAIN_LEFT_MAIN(0, "rio"),
+    DRIVETRAIN_LEFT_FOLLOWER(1, "rio"),
+    DRIVETRAIN_RIGHT_MAIN(2, "rio"),
+    DRIVETRAIN_RIGHT_FOLLOWER(3, "rio");
 
     public final int id;
     public final String bus;
---
+++ src/main/java/frc/robot/subsystems/Drivetrain/Drivetrain.java
@@ -0,0 +1,44 @@
+package frc.robot.subsystems.Drivetrain;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.units.Units;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.lib.component.ComponentSubsystem;
+import frc.lib.io.motor.MotorIO;
+import frc.lib.io.motor.setpoints.VoltageSetpoint;
+
+public class Drivetrain extends ComponentSubsystem {
+    private final DifferentialDrive drive;
+    private final MotorIO leftMotor;
+    private final MotorIO rightMotor;
+
+    public Drivetrain() {
+        leftMotor = DrivetrainConstants.getLeftMotorIO();
+        rightMotor = DrivetrainConstants.getRightMotorIO();
+
+        drive = new DifferentialDrive(this::driveLeft, this::driveRight);
+    }
+
+    private void driveLeft(double leftSpeed) {
+        leftMotor.applySetpoint(new VoltageSetpoint(Units.Volts.of(leftSpeed * 11.0)));
+    }
+
+    private void driveRight(double rightSpeed) {
+        rightMotor.applySetpoint(new VoltageSetpoint(Units.Volts.of(rightSpeed * 11.0)));
+    }
+
+    public Command arcadeDrive(DoubleSupplier forward, DoubleSupplier rotation) {
+        return Commands.run(() -> {
+            this.drive.arcadeDrive(forward.getAsDouble(), rotation.getAsDouble());
+        }, this);
+    }
+
+    public Command tankDrive(DoubleSupplier left, DoubleSupplier right) {
+        return Commands.run(() -> {
+            this.drive.tankDrive(left.getAsDouble(), right.getAsDouble());
+        }, this);
+    }
+}
