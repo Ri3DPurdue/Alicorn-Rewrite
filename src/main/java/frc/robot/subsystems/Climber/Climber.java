a9fde5a5674333e80a22149ce09ba38d02cdce5c
Daniel Quednow <danquednow@gmail.com>
2025-11-01 16:23:36 -0400

    Set start position to min position

--- src/main/java/frc/robot/subsystems/Climber/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber/Climber.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems.Climber;
 
-import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.lib.component.ComponentSubsystem;
@@ -15,7 +14,7 @@ public class Climber extends ComponentSubsystem {
         climber = registerComponent("Climber", new ServoMotorComponent<>(
             ClimberConstants.getMotorIO(),
             ClimberConstants.converter.toAngle(ClimberConstants.positionEpsilon),
-            Units.Radians.of(0)
+            ClimberConstants.minPosition
         ));
     }
 
