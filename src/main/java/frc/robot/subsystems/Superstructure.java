d83bc4a92dc01681c3631caab94078491ac59d66
Daniel Quednow <danquednow@gmail.com>
2025-11-01 14:48:22 -0400

    Add drive commands to superstructure

--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -1,12 +1,16 @@
 package frc.robot.subsystems;
 
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.lib.Util.logging.Loggable;
 import frc.lib.Util.logging.Logger;
+import frc.robot.subsystems.Drivetrain.Drivetrain;
 import frc.robot.subsystems.ExampleIntake.ExampleIntake;
 
 public class Superstructure implements Loggable {
     public final ExampleIntake intake = new ExampleIntake();
+    public final Drivetrain drivetrain = new Drivetrain();
 
     public Command intake() {
         return intake.intake().withName("Intake");
@@ -18,6 +22,14 @@ public class Superstructure implements Loggable {
 
     public Command stow() {
         return intake.stow().withName("Stow");
+    }
+
+    public Command arcadeDrive(DoubleSupplier forward, DoubleSupplier rotation) {
+        return drivetrain.arcadeDrive(forward, rotation);
+    }
+
+    public Command tankDrive(DoubleSupplier left, DoubleSupplier right) {
+        return drivetrain.tankDrive(left, right);
     }
 
     @Override
